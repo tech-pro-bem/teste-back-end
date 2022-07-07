69da5b0d8534c8d2504ffa2c64ab676d46aec69b
BrunaCelestino <brunaccelestino@gmail.com>
2022-07-06 23:49:46 -0300

    feat: create login function

---
+++ src/controllers/authController.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const AdminSchema = require('../models/adminSchema');
+
+const SECRET = process.env.SECRET
+
+const login = (req, res) => {
+  try {
+    const validateLogin = AdminSchema.findOne(
+      { email: req.body.email },
+      (error, admin) => {
+        if (!admin) {
+          return res.status(401).json({
+            message: 'It was not possible to login',
+            details:
+              'Check your e-mail and password before trying to login again',
+          });
+        }
+        const validPassword = bcrypt.compareSync(
+          req.body.password,
+          admin.password,
+        );
+
+        if (!validPassword) {
+          return res.status(401).json({
+            message: 'It was not possible to login',
+            details:
+              'Check your e-mail and password before trying to login again',
+          });
+        }
+        if (error) {
+          res.status(500).json({ message: error.message });
+        }
+        const token = jwt.sign({ adminEmail: admin.email }, SECRET);
+
+        AdminSchema.findByIdAndUpdate(admin.email, { token });
+
+        return res.status(200).json({
+          message: 'Login authorized.',
+          email: admin.email,
+          token,
+        });
+      },
+    );
+    return validateLogin;
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  login,
+};
